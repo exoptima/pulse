6d08f0d71c91a5ff57693aa530ada496d05886c6
Quang Chien <chienglory@gmail.com>
2019-04-07 00:21:00 +0700

    Modify text to test

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -18,6 +18,7 @@ export default class HomePage extends React.PureComponent {
   render() {
     return (
       <h1>
+        Thats Awesome
         <FormattedMessage {...messages.header} />
       </h1>
     );
