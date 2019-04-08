e0f933efa5c77783004ae727f97d4216828cb23c
exoptima <hamedansari@Hameds-MacBook-Pro.local>
2019-04-07 22:17:49 -0400

    Hello World again

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -16,7 +16,7 @@ export default class HomePage extends React.PureComponent {
   render() {
     return (
       <div>
-        <h1>Hello World again!</h1>
+        <h1>Hello World again!!</h1>
       </div>
     );
   }
